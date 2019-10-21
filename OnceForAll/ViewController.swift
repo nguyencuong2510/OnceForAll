0258965ba5a0ba0fc521d4f2d4e57f7e9ae50a0a
admin <cuong251095@gmail.com>
2019-10-22 01:01:47 +0700

    Initial Commit

---
+++ OnceForAll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnceForAll
+//
+//  Created by admin on 10/22/19.
+//  Copyright © 2019 cuongnv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
